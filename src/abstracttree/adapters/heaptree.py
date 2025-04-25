b6a85644cfad71c10ae31108efbf987931c2f8aa
Verweijen <lf.verweijen@cbs.nl>
2025-04-25 13:06:12 +0200

    Make HeapTree immutable

--- src/abstracttree/adapters/heaptree.py
+++ src/abstracttree/adapters/heaptree.py
@@ -1,6 +1,9 @@
-from typing import Collection, Optional
+from collections.abc import MutableSequence
+from typing import Collection, Optional, TypeVar
 
 from ..mixins import BinaryTree
+
+D = TypeVar("D")
 
 
 class HeapTree(BinaryTree):
@@ -20,59 +23,73 @@ class HeapTree(BinaryTree):
     └─ 2 → 3
     """
 
-    __slots__ = "heap", "index"
+    __slots__ = "_heap", "_index"
 
-    def __init__(self, heap=None, index=0):
+    def __init__(self, heap: MutableSequence[D] = None, index: int = 0):
         if heap is None:
             heap = []
-        self.heap = heap
-        self.index = index
+        self._heap = heap
+        self._index = index
 
     def __repr__(self):
-        return f"{type(self).__qualname__}{self.heap, self.index}"
+        return f"{type(self).__qualname__}{self._heap, self._index}"
 
     def __str__(self):
         try:
-            return f"{self.index} → {self.value}"
+            return f"{self._index} → {self.value}"
         except IndexError:
             return repr(self)
 
     @property
     def nid(self):
-        return (id(self.heap) << 32) | self.index
+        return (id(self._heap) << 32) | self._index
+
+    @property
+    def heap(self) -> MutableSequence[D]:
+        return self._heap
+
+    @property
+    def index(self):
+        return self._index
+
+    @property
+    def value(self) -> D:
+        return self._heap[self._index]
 
     def __eq__(self, other):
-        """Nodes should refer to the same heap (identity) with the same index."""
-        return self.heap is other.heap and self.index == other.index
+        """Nodes should refer to the same _heap (identity) with the same _index."""
+        return (isinstance(other, HeapTree)
+                and self._heap is other._heap
+                and self._index == other._index)
+
+    def __hash__(self):
+        """Hashes by id(list) and _index."""
+        return hash((id(self._heap), self._index))
 
     @property
     def children(self) -> Collection["HeapTree"]:
         return [
-            HeapTree(self.heap, i)
-            for i in range(2 * self.index + 1, 2 * self.index + 3)
-            if i < len(self.heap)
+            HeapTree(self._heap, i)
+            for i in range(2 * self._index + 1, 2 * self._index + 3)
+            if i < len(self._heap)
         ]
 
     @property
     def left_child(self) -> Optional["HeapTree"]:
-        i = 2 * self.index + 1
-        if i < len(self.heap):
-            return HeapTree(self.heap, i)
+        i = 2 * self._index + 1
+        if i < len(self._heap):
+            return HeapTree(self._heap, i)
 
     @property
     def right_child(self) -> Optional["HeapTree"]:
-        i = 2 * self.index + 2
-        if i < len(self.heap):
-            return HeapTree(self.heap, i)
+        i = 2 * self._index + 2
+        if i < len(self._heap):
+            return HeapTree(self._heap, i)
 
     @property
     def parent(self) -> Optional["HeapTree"]:
-        n = self.index
+        n = self._index
         if n != 0:
-            return HeapTree(self.heap, (n - 1) // 2)
+            return HeapTree(self._heap, (n - 1) // 2)
         else:
             return None
-
-    @property
-    def value(self):
-        return self.heap[self.index]
